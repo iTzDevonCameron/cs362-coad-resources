fe2050492bcb7ed90239f99f21f95edc0659d69b
Lorenzo Ramos <52989967+Lorenzorb@users.noreply.github.com>
2026-02-15 20:07:11 -0800

    Add files via upload

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,18 @@
 FactoryBot.define do
-  sequence(:email) { |n| "fakeuser#{n}@fakedomain#{n}.com" }
   factory :user do
-    email
-    password { "password123" }
-    password_confirmation { "password123" }
+    sequence(:email) { |n| "org#{n}@example.com" }
+    password { "Fake_password" }
+
+
+    before(:create) { |user| user.skip_confirmation!}
+
+    trait :organization_approved do
+      role { :organization }
+      organization_id { create(:organization).id }
+    end
+
+    trait :admin do
+      role { :admin }
+    end
   end
 end
