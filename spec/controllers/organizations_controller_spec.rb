8e3559fc5b66e82a4b054fcb8400150d1ac01806
Lorenzo Ramos <52989967+Lorenzorb@users.noreply.github.com>
2026-02-22 10:13:28 -0800

    Not finished but added tests
    Need to add the more complex tests, I think I missed some of it because I was out sick last week

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,4 +2,33 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
 
+    let(:admin) { create(:user, role: :admin) }
+    let(:non_admin) { create(:user) }
+
+    let(:approved_org) { create(:organization, status: :approved) }
+    let(:submitted_org) { create(:organization, status: :submitted) }
+
+
+    describe "GET index" do
+        it "redirects to sign in when not logged in" do
+            get :index
+            expect(response).to redirect_to(new_user_session_path)
+        end
+
+        it "renders the page when non-admin logs in" do
+            sign_in non_admin
+            get :index
+            expect(response).to have_http_status(:ok)
+        end
+    end
+
+
+    describe "GET show" do
+      it "allows admin to see the organization" do
+        sign_in admin
+        get :show, params: { id: submitted_org.id }
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
 end
--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,4 +2,51 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
+  let(:admin) { create(:user, role: :admin) }
+  let(:non_admin) { create(:user) }
+
+  let!(:category) { create(:resource_category, name: "test category") }
+
+  describe "GET index" do
+    it "redirects to sign in when logged out" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it "blocks non-admin" do
+      sign_in non_admin
+      get :index
+      expect(response).to redirect_to(dashboard_path)
+    end
+
+    it "renders for the admin" do
+      sign_in admin
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET show" do
+    it "renders for admin" do
+      sign_in admin
+      get :show, params: { id: category.id }
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET new" do
+    it "renders for admin" do
+      sign_in admin
+      get :new
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "GET edit" do
+    it "renders for admin" do
+      sign_in admin
+      get :edit, params: { id: category.id }
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
